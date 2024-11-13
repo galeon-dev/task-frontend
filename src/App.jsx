b82c9a68d7364e571aadcb4bb3aeec41be0cf91b
Peter <ahoj@peterbartos.sk>
2024-11-13 09:33:07 +0100

    Added prettier, medications.json, base UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Card } from '@mui/material';
+import medicationsDB from './data/medications.json';
+
+console.log(medicationsDB); // Medications data
+
 function App() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+    return (
+        <div>
+            <Card style={{ margin: 20, padding: 15, width: '50%' }}>Hello World!</Card>
+        </div>
+    );
 }
 
-export default App
+export default App;
